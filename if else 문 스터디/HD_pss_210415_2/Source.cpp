e52b6a7b6bac6af57659c88528045cfec590547d
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-16 00:50:00 +0900

    문2)erorr메세지를 못지웠다. .. ㅠㅠ

--- if else 문 스터디/HD_pss_210415_2/Source.cpp
+++ if else 문 스터디/HD_pss_210415_2/Source.cpp
@@ -3,29 +3,29 @@ void main() {
 	
 	
 	int i = 1;
-	int input ;
+	int input =1;
 
 	printf("1~9의 수를 입력하면 구구단이 출력됩니다.\n");
 	printf("0을 입력하면 프로그램이 종료됩니다.\n");
 	scanf_s("%d",&input);
 	
-	while (i <10) {
-		printf("%d*%d=%d\n", input,i, input * i);
+	while (i <= 9) {
+		printf("%d*%d=%d\n", input, i, input * i);
 		i += 1;
+		if (input == 0) {
+			printf("종료");
+			break;
+		}
+
+
+		else if (i > 10) {
+			printf("Erorr");
+
+		}
+
+		else (input > 10); {
+			printf("Erorr");
+		}input++;
 	}
-	if (input == 0) {
-		printf("종료");
-		
-
-	}
-
-	else if (i > 9) {
-		printf("Erorr");
-
-	}
-
-	else if (input > 9) {
-					printf("Erorr");	
-				}
 	fgetc(stdin);
 }
