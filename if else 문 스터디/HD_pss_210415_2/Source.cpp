b1eb19d5e3804b370ed7eb1230a76e0fbc878a81
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-16 00:04:42 +0900

    문2) 계속풀이중. ..

--- if else 문 스터디/HD_pss_210415_2/Source.cpp
+++ if else 문 스터디/HD_pss_210415_2/Source.cpp
@@ -1,23 +1,46 @@
 #include<stdio.h>
 void main() {
-	int i = 1;
-	int j = 1;
-
-	while (i <10) {
-		int i = 1;
-		printf("%d 단\n", i);
-		printf("%d", i);
-		while (j <=i) {
-			int j = 1;
-			printf("%d*%d=%d\n", i, j, i * j);
-			j++;
-		}
-	}i++;
+	
+	
+	int i = 1, j = 0;
+	int input = 0;
 
 	printf("1~9의 수를 입력하면 구구단이 출력됩니다.\n");
 	printf("0을 입력하면 프로그램이 종료됩니다.");
-	printf("\n");
+	scanf_s("%d\n",&input);
 	
 	
+	
+	while (true) {
+		
+			if (input == 0) {
+				printf("종료");
+				
+			}
+			else if (input==1) {
+				j++;
+				if(j>9){
+					i++;
+					j = 1;
+					printf("\n");
+			}
+				if (i > 9) {
+					printf("Erorr");
+					
+				}
+				
+				else {
+					i = input;
+					j++;
+					if (j > 9) 
+					printf("Erorr");
+						
+				}
+
+			printf("%d*%d=%d", i, j, (i*j));
+		}
+		
+	}
+
 	fgetc(stdin);
 }
