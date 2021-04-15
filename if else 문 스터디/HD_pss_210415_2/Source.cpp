ff4b3c3aca7a8c6c8eae024e5f0819ed02dae0c3
DESKTOP-BHJPBEP\gravi <gravityfree01@gmail.com>
2021-04-16 01:04:30 +0900

    문2) erorr 문자 삭제 완료

--- if else 문 스터디/HD_pss_210415_2/Source.cpp
+++ if else 문 스터디/HD_pss_210415_2/Source.cpp
@@ -8,23 +8,25 @@ void main() {
 	printf("1~9의 수를 입력하면 구구단이 출력됩니다.\n");
 	printf("0을 입력하면 프로그램이 종료됩니다.\n");
 	scanf_s("%d",&input);
-	
 	while (i <= 9) {
 		printf("%d*%d=%d\n", input, i, input * i);
 		i += 1;
-		if (input == 0) {
-			printf("종료");
-			break;
-		}
+		while (i >10) {
+			
+			if (input == 0) {
+				printf("종료");
+				break;
+			}
 
 
-		else if (i > 10) {
-			printf("Erorr");
+			else if (i > 10) {
+				printf("Erorr");
 
-		}
+			}
 
-		else (input > 10); {
-			printf("Erorr");
+			else (input > 10); {
+				printf("Erorr");
+			}
 		}
 	}
 	fgetc(stdin);
